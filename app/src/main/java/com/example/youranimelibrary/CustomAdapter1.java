12b8f6dea7f22d164d179f92357ae6157ed3273b
LeekOnionRings <156482704+LeekOnionRings@users.noreply.github.com>
2026-02-22 00:09:09 -0500

    Commit 9 - Added Recycler View to existing_user_xml. Fixed CustomAdapter1 (base template) so it has no errors.

--- app/src/main/java/com/example/youranimelibrary/CustomAdapter1.java
+++ app/src/main/java/com/example/youranimelibrary/CustomAdapter1.java
@@ -1,43 +1,40 @@
 package com.example.youranimelibrary;
 
 import static androidx.core.content.ContextCompat.startActivity;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
+import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+
+
 
 public class CustomAdapter1 extends RecyclerView.Adapter<CustomAdapter1.MyHandler> {
+
     public static class MyHandler extends RecyclerView.ViewHolder {
 
-        TextView username;
         public MyHandler(@NonNull View itemView) {
             super(itemView);
         }
+    }
 
-        @NonNull
-        @Override
-        public MyHandler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    @NonNull
+    @Override
+    public MyHandler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 
-            View theInflatedLayout = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.user_layout, parent, false);
-
-            return new MyHandler(theInflatedLayout);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyHandler holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
+    @Override
+    public void onBindViewHolder(@NonNull MyHandler holder, int position) {
 
     }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
 }
