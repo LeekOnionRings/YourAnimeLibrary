724417e33b2a75b0d46af384d067dd3c64e933f8
LeekOnionRings <156482704+LeekOnionRings@users.noreply.github.com>
2026-02-22 00:25:49 -0500

    Commit 10 - (CustomAdapter1) Added variables to handler and constructor.

--- app/src/main/java/com/example/youranimelibrary/CustomAdapter1.java
+++ app/src/main/java/com/example/youranimelibrary/CustomAdapter1.java
@@ -5,20 +5,55 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
 
 
 public class CustomAdapter1 extends RecyclerView.Adapter<CustomAdapter1.MyHandler> {
 
     public static class MyHandler extends RecyclerView.ViewHolder {
 
+        TextView usernameTextView;
+        Button viewAnimesButton;
+        Button editUserButton;
+        Button deleteUserButton;
+
         public MyHandler(@NonNull View itemView) {
             super(itemView);
+            usernameTextView = itemView.findViewById(R.id.usernameTextView);
+            viewAnimesButton = itemView.findViewById(R.id.viewAnimesButton);
+            editUserButton = itemView.findViewById(R.id.editUserButton);
+            deleteUserButton = itemView.findViewById(R.id.deleteUserButton);
+
         }
+
+        public TextView getUsernameTextView() {
+            return usernameTextView;
+        }
+
+        public Button getViewAnimesButton() {
+            return viewAnimesButton;
+        }
+
+        public Button getEditUserButton() {
+            return editUserButton;
+        }
+
+        public Button getDeleteUserButton() {
+            return deleteUserButton;
+        }
+
     }
+
+    List<User> userList;
+    public CustomAdapter1(List<User> userList) {
+        this.userList = userList;
+    }
+
 
     @NonNull
     @Override
@@ -26,14 +61,16 @@ public class CustomAdapter1 extends RecyclerView.Adapter<CustomAdapter1.MyHandle
         return null;
     }
 
+
     @Override
     public void onBindViewHolder(@NonNull MyHandler holder, int position) {
 
     }
 
+
     @Override
     public int getItemCount() {
-        return 0;
+        return userList.size();
     }
 
 
